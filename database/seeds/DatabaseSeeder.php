0d8d4f233a502f76a9a623a9ab10c21095acc86e
Alberto Rosas - Bcrypt <alberto.rsesc@protonmail.com>
2018-03-20 23:54:28 -0700

    Using Larancer from LaravelDaily

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        
+        $this->call(ClientStatusSeed::class);
+        $this->call(ClientSeed::class);
+        $this->call(CurrencySeed::class);
+        $this->call(ProjectStatusSeed::class);
+        $this->call(ProjectSeed::class);
+        $this->call(IncomeSourceSeed::class);
+        $this->call(NoteSeed::class);
+        $this->call(RoleSeed::class);
+        $this->call(TransactionTypeSeed::class);
+        $this->call(TransactionSeed::class);
+        $this->call(UserSeed::class);
+        $this->command->info('Seeds finished successfully. Admin email: admin@admin.com, Password: password');
+
     }
 }
